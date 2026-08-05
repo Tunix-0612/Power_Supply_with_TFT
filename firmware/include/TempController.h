eab73a7509cf930a91dd479ba0a2871b2846f4d8
Tunix-0612 <tunahan--bayraktar@hotmail.com>
2026-08-05 22:09:51 +0300

    v0.7.3 Alpha - NOT TESTED

--- firmware/include/TempController.h
+++ firmware/include/TempController.h
@@ -6,19 +6,17 @@
 class TempController
 {
   private:
-    uint8_t currentFanPWM = 0;            // Anlık uygulanan PWM değeri
-    uint8_t currentAggression = 0;        // Anlık agresyon seviyesi (0, 1, 2, 3)
+    uint8_t currentFanPWM = 0;
+    uint8_t currentAggression = 0;
         
-    unsigned long highPowerStartTime = 0; // 50W barajının aşıldığı an
-    bool isHighPowerActive = false;       // Güç barajı aşıldı mı?
+    unsigned long highPowerStartTime = 0;
+    bool isHighPowerActive = false;
 
     int readTemperature();
 
-    // Puanlama Fonksiyonları
     uint8_t getPowerAggression(float currentPower);
     uint8_t getTempAggression(int currentTemp);
-        
-    // Donanım Fonksiyonu
+
     void applyAggression(uint8_t level);
 
     public:
